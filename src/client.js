25a05ad0637999aa0478a812c260f999ce139db2
kurono <kurono@riseup.net>
2025-08-26 12:09:11 -0600

    Improve login message

--- src/client.js
+++ src/client.js
@@ -219,9 +219,8 @@ app.get('/callback', async (req, res) => {
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
             </head>
             <body class="container py-5">
-                <h1 class="mb-4 text-success">¡Token de acceso recibido!</h1>
-                <p><strong>Tipo de Token:</strong> ${token_type}</p>
-                <p><strong>Expira en:</strong> ${expires_in} minutos</p>
+                <h1 class="mb-4 text-success">¡Usuario autenticado, bienvenido!</h1>
+                <p><strong>Sesión expira en:</strong> ${expires_in} minutos</p>
                 <div class="mb-3">
                     <p class="mb-1"><strong>Token:</strong></p>
                     <pre class="bg-light p-3 rounded">${JSON.stringify(parseJwt(access_token), null, 2)}</pre>
